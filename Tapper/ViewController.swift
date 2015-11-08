99990a993b2b18ea8238b2f60133100973551139
Bartłomiej Dietl <bartlomiejdietl@Barts-MacBook-Pro.local>
2015-11-08 20:49:28 +0100

    added func. Final one

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Variables
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    
+    //Outlets
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var howManyTaps: UITextField!
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var tapCouter: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +32,55 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func tapButtonPressed(sender: UIButton!){
+        currentTaps++
+        updateTapCouter()
+        
+        if isGameOver() {
+            restartGame()
+        }
+        
+    }
+    
+    @IBAction func onPlayButtonPressed(sender: UIButton!){
+        
+        if howManyTaps.text != nil && howManyTaps.text != "" {
+            logoImageView.hidden = true
+            startButton.hidden = true
+            howManyTaps.hidden = true
+            
+            tapButton.hidden = false
+            tapCouter.hidden = false
+            
+            maxTaps = Int(howManyTaps.text!)!
+            currentTaps = 0
+            
+            updateTapCouter()
+            
+        }
+    }
+    func updateTapCouter() {
+        tapCouter.text = "\(currentTaps) Taps"
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    func restartGame() {
+            maxTaps = 0
+            howManyTaps.text = ""
+            logoImageView.hidden = false
+            startButton.hidden = false
+            howManyTaps.hidden = false
+            
+            tapButton.hidden = true
+            tapCouter.hidden = true
+        }
 
 }
 
